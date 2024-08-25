9bbe7cdc78fc8b5d3a56dbc04971d3d0c9d7ae54
Ana C M Damasceno <acmd9215@gmail.com>
2024-08-25 12:08:48 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -16,6 +16,7 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
     'Level2Bg1': 0,
     'Level2Bg2': 0,
     'Level2Bg3': 0,
@@ -38,6 +39,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -56,6 +62,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -74,6 +85,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot': 1,
     'Player2': 3,
@@ -93,6 +109,8 @@ ENTITY_SHOT_DELAY = {
 
 EVENT_ENEMY = pygame.USEREVENT + 1
 
+EVENT_TIMEOUT = pygame.USEREVENT + 2
+
 # M
 MENU_OPTION = ['NEW GAME 1P', 'NEW GAME 2P - COOPERATIVE', 'NEW GAME 2P - COMPETITIVE', 'SCORE', 'EXIT']
 
@@ -109,6 +127,11 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 # S
 SPAWN_TIME = 4000
+
+# T
+TIMEOUT_LEVEL = 20000
+TIMEOUT_STEP = 100
+
 # W
 WIN_WIDTH = 567
 WIN_HEIGHT = 324
